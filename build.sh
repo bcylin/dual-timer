4d62759b1a2c006436073e1ea4e90bc963c10017
Ben <b.cylin@gmail.com>
2013-04-13 21:43:49 +0800

    Update script to deploy gh-pages

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 readonly DEPLOY="deploy"
+readonly BRANCH="gh-pages"
 
 if [ ! -d "./${DEPLOY}" ]; then
     mkdir "./${DEPLOY}"
@@ -17,3 +18,21 @@ cp -fv ./index.html "./${DEPLOY}/"
 lessc -x ./css/timer.css > "./${DEPLOY}/css/timer.css"
 uglifyjs2 -v ./js/timer.js -o "./${DEPLOY}/js/timer.js"
 echo "" >> "./${DEPLOY}/js/timer.js"
+
+echo "\n""Checkout ${BRANCH}""\n"
+git checkout ${BRANCH}
+
+if [ ! -d ./css/ ]; then
+    mkdir ./css/
+fi
+
+if [ ! -d ./js/ ]; then
+    mkdir ./js/
+fi
+
+cp -rfv "./${DEPLOY}/css/"* ./css/
+cp -rfv "./${DEPLOY}/js/"* ./js/
+cp -fv "./${DEPLOY}/index.html" ./index.html
+
+git add .
+echo "\n""Ready to commit""\n"
